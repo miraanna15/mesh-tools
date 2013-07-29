349ae2c8d17ab5fc34688e030b0057d02c0aa3f2
Adam Reeve <aree035@aucklanduni.ac.nz>
2013-07-29 12:06:56 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+        name='exfile',
+        version='0.1',
+        py_modules=['exfile'],
+        author = 'Adam Reeve',
+        install_requires = ['numpy']
+      )
