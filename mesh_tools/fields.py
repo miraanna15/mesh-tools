c874458d9cde7de509571085aa0aee5589e02a12
Prasad <psam012@aucklanduni.ac.nz>
2019-07-31 22:58:30 +1200

    Updating field evaluation functions

--- mesh_tools/fields.py
+++ mesh_tools/fields.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generate_xi_on_face(face, value, num_points=4, dim=3):
+def generate_xi_on_face(face, value, num_points=[4, 4], dim=3):
     """
     Generate a grid of points within each element
 
@@ -8,19 +8,22 @@ def generate_xi_on_face(face, value, num_points=4, dim=3):
     face -- face to evaluate points on at the specified xi value
     dim -- the number of xi directions
     """
+    num_points = np.atleast_1d(num_points)
+    if len(num_points)==1:
+        num_points = np.array([num_points[0], num_points[0]])
 
     if dim == 3:
         if face == "xi1":
             xi1 = [value]
-            xi2 = np.linspace(0., 1., num_points)
-            xi3 = np.linspace(0., 1., num_points)
+            xi2 = np.linspace(0., 1., num_points[0])
+            xi3 = np.linspace(0., 1., num_points[1])
         elif face == "xi2":
-            xi1 = np.linspace(0., 1., num_points)
+            xi1 = np.linspace(0., 1., num_points[0])
             xi2 = [value]
-            xi3 = np.linspace(0., 1., num_points)
+            xi3 = np.linspace(0., 1., num_points[1])
         elif face == "xi3":
-            xi1 = np.linspace(0., 1., num_points)
-            xi2 = np.linspace(0., 1., num_points)
+            xi1 = np.linspace(0., 1., num_points[0])
+            xi2 = np.linspace(0., 1., num_points[1])
             xi3 = [value]
         X, Y, Z = np.meshgrid(xi1, xi2, xi3)
         xi = np.array([
@@ -28,8 +31,8 @@ def generate_xi_on_face(face, value, num_points=4, dim=3):
             Y.reshape((Y.size)),
             Z.reshape((Z.size))]).T
     elif dim == 2:
-        xi1 = np.linspace(0., 1., num_points)
-        xi2 = np.linspace(0., 1., num_points)
+        xi1 = np.linspace(0., 1., num_points[0])
+        xi2 = np.linspace(0., 1., num_points[1])
         X, Y = np.meshgrid(xi1, xi2)
         xi = np.array([
             X.reshape((X.size)),
